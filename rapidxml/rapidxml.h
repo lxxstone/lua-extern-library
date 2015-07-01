4f84452d01207e44c6a3ad44b1d854024f4d9d76
stonelxx <stonelxx@foxmail.com>
2015-07-01 11:01:54 +0800

    add rapidxml

---
+++ rapidxml/rapidxml.h
@@ -0,0 +1,18 @@
+#ifndef xerces_H_
+#define xerces_H_
+
+#ifndef LIBNAME
+#define LIBNAME "rapidxml"
+#endif
+
+#ifndef LIBVER
+#define LIBVER "0.1"
+#endif
+
+extern "C" {
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+};
+
+#endif
